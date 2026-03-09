4961a610ffe5a06796b9bdfee82e39a8de7ce94b
lzitaliancraftbeer-coder <lzitaliancraftbeer@gmail.com>
2026-03-09 21:57:05 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,3 +133,34 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 });
+
+const counters = document.querySelectorAll('.counter');
+
+const speed = 200;
+
+counters.forEach(counter => {
+
+const updateCount = () => {
+
+const target = +counter.getAttribute('data-target');
+const count = +counter.innerText;
+
+const increment = target / speed;
+
+if(count < target){
+
+counter.innerText = Math.ceil(count + increment);
+
+setTimeout(updateCount,20);
+
+}else{
+
+counter.innerText = target;
+
+}
+
+};
+
+updateCount();
+
+});
